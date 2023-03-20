f8d601fe9b8354765450279ae19c183bc8f5824d
Matheusdevlp <matheusphetrabalho@gmail.com>
2023-03-19 22:01:36 -0300

    Validacao nos campos de criacao de novo usuario

--- src/main/java/com/valdir/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/valdir/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -2,14 +2,17 @@ package com.valdir.helpdesk.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.valdir.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.valdir.helpdesk.services.exceptions.ObjectnotFoundException;
-import com.valdir.helpdesk.services.exceptions.StandardError;
+
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -28,5 +31,18 @@ public class ResourceExceptionHandler {
 		StandardError error = new StandardError(System.currentTimeMillis(),HttpStatus.NOT_FOUND.value(), "Violacao de dados", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, HttpServletRequest request){
+		
+	ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Validation error", "Erro na validacao dos campos", request.getRequestURI());
+	
+	for(FieldError x : ex.getBindingResult().getFieldErrors()){
+		errors.addError(x.getField(), x.getDefaultMessage());
+	}
+		
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 }
