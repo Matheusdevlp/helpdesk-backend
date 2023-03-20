f8f81a2ccb3e47bcb3eb3f030d1ea5f8f30b71aa
Matheusdevlp <matheusphetrabalho@gmail.com>
2023-03-19 22:37:45 -0300

    metodo Put para tecnico implementado

--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.valdir.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,7 +40,17 @@ public class TecnicoService {
 		
 		return repository.save(newObj);
 	}
-
+	
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
+	
+	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent() && obj.get().getId()!=objDTO.getId()) {
@@ -51,3 +63,4 @@ public class TecnicoService {
 		}
 	}
 }
+
